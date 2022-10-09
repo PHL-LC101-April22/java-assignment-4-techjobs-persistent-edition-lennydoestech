96372a18596a2b2b621c5c62bde3e8c448ed4454
lennydoestech <76795847+lennydoestech@users.noreply.github.com>
2022-10-09 13:54:05 +0200

    1 test failed in task 4. Need help please.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 -- Part 1: Test it with SQL
-
+id INT
+Employer VARCHAR
+Name VARCHAR
+Skills VARCHAR
 -- Part 2: Test it with SQL
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+LEFT JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id is not null
+ORDER BY name ASC;
